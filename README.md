2258e2ff35a10040b179143f52c123b01625bfff
gamykla <admin@gamykla.com>
2017-09-22 15:50:21 -0400

    First commit

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # machine-learning-map-reduce
+
 Solving some machine learning problems with map-reduce
+
+## What is this?
+Many popular machine learning algorithms can be solved with map reduce as discussed in this paper http://papers.nips.cc/paper/3150-map-reduce-for-machine-learning-on-multicore.pdf
+
+Furthermore, it is demonstrated that these common algorithms can scale linearly to the number of processors involved in the computation.
+
+To take advantage of parallelism, algorithms need to be re-formed so that they can be expressed as map reduce operations. This project will demonstrate some of those techniques, starting with simpler problems such as logistic regression with gradient descent expressed as a map reduce computation
+
+## Running the examples
+First install the requirements
+```
+pip install -r requirements.txt
+```
+Next you'll want to start your 'cluster'. I have 8 cores so i'll setup 8 workers:
+```
+ipcluster start -n 8
+```
+
+NB: ipyparallel can run locally, or remotely.. You can build up a cluster to meet your needs, see http://ipyparallel.readthedocs.io/en/latest/ for more detail.
+
+Once the cluster has started you can run the python examples.
---
+++ linear_regression_mapreduce.py
@@ -0,0 +1,123 @@
+import ipyparallel as ipp
+
+client = ipp.Client()
+dview = client[:]
+
+with dview.sync_imports():
+    # this way, all of these imports are available to workers
+    import pandas
+    import numpy
+    import math
+    from sklearn.model_selection import train_test_split
+
+
+def get_labels(data_frame):
+    return numpy.asarray(data_frame['y'])
+
+
+def get_features(data_frame):
+    return data_frame.drop('y', axis=1)
+
+
+def load_data_frame():
+    data_frame = pandas.read_csv('data/linear-regression.txt', delimiter=",")
+    # add intercept x=1
+    data_frame.insert(0, 'i', 1)
+    return data_frame
+
+
+def compute_cost_function(theta, X, y):
+    """ for given model parameters 'theta' compute the model
+    cost for features 'X' and labels 'y' """
+    m = len(y)
+    total_sum = 0
+
+    for i, x in X.iterrows():
+        x = x.as_matrix()
+        total_sum += math.pow((h(theta, x) - y[i]), 2)
+
+    return (1.0/(2*m)) * total_sum
+
+
+def distributed_sum():
+    """
+    one part of a sum that is distributred across the cluster.
+    Compute: sum( (H(x_i) - y_i)*x_ij )
+    the result will then be reduced and multiplied by 1/m to form a derivative term.
+    must be pushed: j, theta
+    scattered: X, y
+    """
+
+    def h(x):
+        return numpy.asscalar(theta.dot(x))
+
+    i = 0
+    sum_j = 0
+    for _, x_i in X.iterrows():
+        sum_j += (h(x_i) - y[i]) * x_i[j]
+        i += 1
+
+    return sum_j
+
+
+def gradient_descent(theta, alpha, total_iterations, training_set_size):
+    """
+    Gradient descent - this is the algorithm that finds optimal parameters for our linear regression model.
+    """
+    len_theta = len(theta)
+
+    # repeat for total_iterations
+    for _ in range(0, total_iterations):
+        temp_theta = numpy.zeros(len_theta)
+
+        for j in range(0, len_theta):
+            dview.push({"theta": theta, "j": j})
+            ar = dview.apply_async(distributed_sum)
+            dview.wait(ar)
+            total_sum = reduce((lambda x, y: x + y), ar.get())
+            print total_sum
+            derivative_j = (1.0 / float(training_set_size)) * total_sum
+            temp_theta[j] = theta[j] - alpha*derivative_j
+
+        theta = temp_theta
+
+    return theta
+
+
+RANDOM_SEED = 42
+TEST_SET_SIZE = 0.3
+
+
+def main():
+    data_frame = load_data_frame()
+    y = get_labels(data_frame)
+    X = get_features(data_frame)
+    m, feature_vector_size = X.shape # feature vector size includes intercept term (1)
+
+    # partition data set into train and test
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=TEST_SET_SIZE, random_state=RANDOM_SEED)
+
+    print "Features: {}".format(feature_vector_size-1)
+    print "X Size: {}".format(len(X))
+    print "X train size: {}".format(m)
+    print "y train size: {}".format(len(y_train))
+    print "X test size: {}".format(len(X_test))
+    print "y test size: {}".format(len(y_test))
+
+    # Distribute training set across the cluster
+    dview.scatter('X', X_train)
+    dview.scatter('y', y_train)
+
+    theta = numpy.zeros(feature_vector_size)
+
+    # tunable gradient descent parameters
+    alpha = 0.01
+    iterations = 500
+
+    theta = gradient_descent(theta, alpha, iterations, len(y_train))
+    print "trained theta: {}".format(theta)
+
+
+if __name__ == "__main__":
+    main()
+
